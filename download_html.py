7998674c998e7174ded9068167943aa250bd0daa
corey <coreyclip@gmail.com>
2020-08-28 01:07:13 -0700

    begin etl

--- download_html.py
+++ download_html.py
@@ -27,13 +27,14 @@ def wait_for(selector,method, seconds):
 
 
 timestamp = dt.now().strftime("%m-%d-%Y")
-if not os.path.isdir(timestamp):
-    os.mkdir(timestamp)
+if not os.path.isdir(os.path.join('data/scraped_data', timestamp)):
+    os.mkdir(os.path.join('data/scraped_data', timestamp))
 driver = webdriver.Firefox()
 driver.get("http://dashboard.publichealth.lacounty.gov/covid19_surveillance_dashboard/")
 try:
     driver.set_window_size(1099, 694)
     driver.switch_to.frame(0)
+    time.sleep(10)
     wait_for("li:nth-child(5) span", "css", 10)
     # download first table
     driver.find_element(By.CSS_SELECTOR, "li:nth-child(5) span").click()
@@ -43,7 +44,9 @@ try:
         driver.find_element(By.CSS_SELECTOR, "li:nth-child(5) span").click()
         wait_for("DataTables_Table_0_length", "name", 10)
     driver.find_element(By.NAME, "DataTables_Table_0_length").click()
+    time.sleep(3)
     dropdown = driver.find_element(By.NAME, "DataTables_Table_0_length")
+    time.sleep(5)
     dropdown.find_element(By.XPATH, "//option[. = 'All']").click()
     time.sleep(2)
     wait_for("option:nth-child(3)", "css", 10)
@@ -52,7 +55,7 @@ try:
     table = driver.find_element(By.ID, "DataTables_Table_0").get_attribute("outerHTML")
     df = pd.read_html(table)[0]
     df["timestamp"] = dt.now()
-    df.to_csv(os.path.join(timestamp, "LA_County_Covid19_CSA_case_death_table.csv"))
+    df.to_csv(os.path.join(f"data/scraped_data/{timestamp}", "LA_County_Covid19_CSA_case_death_table.csv"))
     driver.find_element(By.CSS_SELECTOR, "li:nth-child(6) span").click()
     try:
         wait_for("DataTables_Table_1_length", "name", 5)
@@ -61,6 +64,7 @@ try:
         wait_for("DataTables_Table_1_length", "name", 10)
     driver.find_element(By.NAME, "DataTables_Table_1_length").click()
     dropdown = driver.find_element(By.NAME, "DataTables_Table_1_length")
+    time.sleep(10)
     dropdown.find_element(By.XPATH, "//option[. = 'All']").click()
     time.sleep(2)
     driver.find_element(By.CSS_SELECTOR, "#DataTables_Table_1_length option:nth-child(3)").click()
@@ -68,8 +72,7 @@ try:
     table = driver.find_element(By.ID, "DataTables_Table_1").get_attribute("outerHTML")
     df = pd.read_html(table)[0]
     df["timestamp"] = dt.now()
-    df.to_csv(os.path.join(timestamp, "LA_County_Covid19_CSA_testing_table.csv"))
-    import pdb; pdb.set_trace()
+    df.to_csv(os.path.join(f"data/scraped_data/{timestamp}", "LA_County_Covid19_CSA_testing_table.csv"))
     driver.quit()
 except Exception as e:
     driver.quit()
