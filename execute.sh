028d155e1ef7b5ee7b7c1fdb9d8ad52557917f35
corey <coreyclip@gmail.com>
2020-09-08 21:58:53 -0700

    added read out of download success

--- execute.sh
+++ execute.sh
@@ -1,5 +1,7 @@
 source venv/bin/activate
+echo "$(date +"%D %T") downloading LA CSA Testing Data"
 CSA_RESPONSE=$(python download_LA_CSA_testing_table.py)
+echo "$(date +"%D %T") downloading LA case death Data"
 CASE_DEATH_RESPONSE=$(python download_LA_case_death_table.py)
 CSA_DOWNLOAD_STATUS=$(echo $CSA_RESPONSE | grep "Download.*")
 CASE_DEATH_DOWNLOAD_STATUS=$(echo $CASE_DEATH_RESPONSE | grep "Download.*")
@@ -10,6 +12,7 @@ then
     echo "CASE_DEATH_DOWNLOAD_STATUS: ${CASE_DEATH_DOWNLOAD_STATUS}"
     exit 1
 else
+    echo "$(date +"%D %T") performing etl transformations and uploading to google sheets"
     git pull
     python transform_upload_to_gsheets.py
     git add .
@@ -17,4 +20,5 @@ else
     git push origin master
 fi
 
+echo "$(date +"%D %T"), ${CSA_DOWNLOAD_STATUS}, ${CASE_DEATH_DOWNLOAD_STATUS}" >> download_record.csv
 
